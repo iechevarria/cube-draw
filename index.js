c942b97ccb1961c0c7f94e1abc68d59f9778fb22
Ivan Echevarria <iechevarria@email.wm.edu>
2017-09-20 14:20:39 -0400

    add simple mouseover behavior

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var CANVAS_WIDTH = 800
 var CANVAS_HEIGHT = 800
 
 var blockSize = 20
+var mouseLoc = [-1, -1]
 
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
@@ -15,7 +16,7 @@ function shadeColor (color, percent) {
     R = (num >> 16) + amt,
     G = (num >> 8 & 0x00FF) + amt,
     B = (num & 0x0000FF) + amt
-  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1)
 }
 
 function drawCube (x, y, wx, wy, h, color) {
@@ -77,39 +78,32 @@ function drawOutline (x, y, wx, wy, h, color) {
 }
 
 function drawGrid (y) {
-  /*
-  context.beginPath
-  context.moveTo(CANVAS_WIDTH / 2, CANVAS_HEIGHT - blockSize)
-  context.lineTo(20, CANVAS_HEIGHT * 0.75 - blockSize + 10)
-  context.lineTo(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2 - blockSize + 10)
-  context.lineTo(CANVAS_WIDTH - blockSize, CANVAS_HEIGHT * 0.75 - 20)
-  context.closePath()
-  context.fillStyle = 'rgba(200, 225, 255, 0.35)'
-  context.fill()
-  */
-
   for (var i = 0; i < 20; i++) {
-    context.moveTo(CANVAS_WIDTH / 2 - 20 * i, CANVAS_HEIGHT - 10 * (i + 2))
-    context.lineTo(CANVAS_WIDTH - 20 * (i + 1), CANVAS_HEIGHT * 0.75 - 10 * (i + 1))
+    context.moveTo(CANVAS_WIDTH / 2 - 20 * i, CANVAS_HEIGHT - 10 * (2 * y + i + 2))
+    context.lineTo(CANVAS_WIDTH - 20 * (i + 1), CANVAS_HEIGHT * 0.75 - 10 * (2 * y + i + 1))
 
-    context.moveTo(CANVAS_WIDTH / 2 + 20 * i, CANVAS_HEIGHT - 10 * (i + 2))
-    context.lineTo(20 * (i + 1), CANVAS_HEIGHT * 0.75 - 10 * (i + 1))
+    context.moveTo(CANVAS_WIDTH / 2 + 20 * i, CANVAS_HEIGHT - 10 * (2 * y + i + 2))
+    context.lineTo(20 * (i + 1), CANVAS_HEIGHT * 0.75 - 10 * (2 * y + i + 1))
   }
 
-  context.strokeStyle = '#acf'
+  context.strokeStyle = '#aaa'
   context.stroke()
 }
 
-drawGrid(0)
+function draw () {
+  context.beginPath()
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-drawCube(200, 200, 20, 20, 20, '#bbbbbb')
-drawCube(260, 230, 20, 20, 20, '#bbbbbb')
-drawCube(260, 230, 20, 20, 20, '#bbbbbb')
+  drawGrid(0)
+  drawOutline(mouseLoc[0], mouseLoc[1], 20, 20, 20, '#4f4')
+}
 
-drawCube(500, 540, 20, 20, 20, '#cccccc')
-drawCube(500, 520, 20, 20, 20, '#cccccc')
-drawCube(520, 490, 20, 20, 20, '#cccccc')
-drawCube(400, 780, 20, 20, 20, '#cccccc')
+function setCursor (e) {
+  mouseLoc[0] = e.offsetX
+  mouseLoc[1] = e.offsetY
+  draw()
+}
 
-drawOutline(500, 500, 20, 20, 20, '#aaccff')
+canvas.addEventListener('mousemove', setCursor)
 
+draw()
